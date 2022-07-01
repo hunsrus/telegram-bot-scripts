2bec0e7abf10cb8b1b10e3d12918e78f3460f7ed
hunsrus <gabie00018@gmail.com>
2022-07-01 15:26:41 -0300

    config file parsing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <string>
 #include <array>
+#include <fstream>
 #include "libtelegram/libtelegram.h"
 
 std::string exec(const char* cmd) {
@@ -22,7 +23,22 @@ std::string exec(const char* cmd) {
 int main(void)
 {
 	std::string response;
-	std::string const token("5286497545:AAHZfebdIGcD8O64XbOMDekiMN2GPyrCbnY");         // in practice you would probably read this from config file or database
+	std::string line, param_name, param_content, token, bot_name, lang;
+	std::ifstream configFile("../config");
+	if(configFile.is_open())
+	{
+		while(std::getline(configFile,line))
+		{
+			int div_pos = line.find_last_of("=");
+			param_name = line.substr(0,div_pos);
+			param_content = line.substr(div_pos+1);
+			if(!param_name.compare("TOKEN")) token = param_content;
+			else if(!param_name.compare("BOT_NAME")) bot_name = param_content;
+			else if(!param_name.compare("LANGUAGE")) lang = param_content;
+			else std::cout << "Unknown parameter in configuration file: " << line << std::endl;
+		}
+		configFile.close();
+	}else std::cout << "Error reading configuration file" << std::endl;
   	telegram::sender sender(token);                                               // create a sender with our token for outgoing messages
   	telegram::listener::poll listener(sender);                                    // create a polling listener which will process incoming requests, polling using the sender
   	listener.set_callback_message([&](telegram::types::message const &message)
@@ -60,24 +76,40 @@ int main(void)
 			if(!file_optional)   // we might not get a file back, so handle this gracefully	
 			{                                                    		
 				sender.send_message(message_chat_id, "Something went wrong and I couldn't download the file.");
-			        return;
+			    return;
 			}
 			auto const &file(*file_optional);
-			//auto file_blob(file.download(token)); ------------------------ NO FUNCIONA STREAM.OPEN()
+			//auto file_blob(file.download(token)); // ------------------------ stream.open() NOT WORKING
+			//momentarily using wget (no crossplatform)
 			std::string url = file.get_url(token).to_string();
+			std::string command = "wget "+url;
 			//std::string url = std::string("api.telegram.org/file/bot" + token + "/"+ *file.file_path);
-			sender.send_message(message_chat_id, url);
-			std::string const filename(std::string("voice_" + file.file_id + ".ogg")); // give it a unique filename
-			std::ofstream filestream(std::string("voice_" + file.file_id + ".ogg"), std::ios::binary); // make sure we write in binary mode
+			//std::string const filename(std::string("voice_" + file.file_id + ".oga")); // give it a unique filename
+			//std::ofstream filestream(std::string("voice_" + file.file_id + ".oga"), std::ios::binary); // make sure we write in binary mode
 			
-			/*filestream.write(file_blob.data(), file_blob.size());                     // write the binary blob to disk
-			if(filestream.fail())
+			std::string file_name = (*file.file_path).substr((*file.file_path).find_last_of("/")+1);
+			
+			//filestream.write(file_blob.data(), file_blob.size());                     // write the binary blob to disk
+			if(system(command.c_str()))
 			{
 				sender.send_message(message_chat_id, "Error descargando el archivo de voz");
 			}else
 			{
-				sender.send_message(message_chat_id, "A la orden");
-			}*/
+				std::string file_wav = file_name.substr(0,file_name.find_last_of(".")+1)+"wav";
+				command = "ffmpeg -i "+file_name+" "+file_wav;
+				system(command.c_str());
+				command = "/usr/local/bin/spchcat "+file_wav+" >> transcript";
+				system(command.c_str());
+				std::string transcript;
+				std::ifstream transcriptFile("transcript");
+				if(transcriptFile.is_open())
+				{
+					std::getline(transcriptFile,transcript);
+				}else transcript = "Error leyendo archivo de transcripción";
+				sender.send_message(message_chat_id, transcript);
+				command = "rm "+file_name+" "+file_wav+" transcript";
+				system(command.c_str());
+			}
 		}
 //		std::string const reply(message_from + " sent \"" + *message.text + "\" to chat id " + std::to_string(message.chat.id)); // each element is a native type - the chat id is an integer
 //	    	sender.send_message(message.chat.id, reply);                                // send our reply from within the callback
