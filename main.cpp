8a607b77c8c009f2cd96e061362f5c9e1f906d5d
hunsrus <gabie00018@gmail.com>
2022-07-04 16:08:09 -0300

    command configuration support

--- main.cpp
+++ main.cpp
@@ -5,9 +5,17 @@
 #include <string>
 #include <array>
 #include <fstream>
+#include <list>
 #include "libtelegram/libtelegram.h"
 
-std::string checknrun(std::string command);
+struct cmd
+{
+	std::string key;
+	std::string script;
+	std::string param;
+};
+
+std::string checknrun(std::string command, std::list<cmd> available_commands, std::string scripts_path);
 int download(std::string url, std::string file_name);
 int convert2wav(std::string file_name);
 int transcribe(std::string file_name);
@@ -28,7 +36,8 @@ std::string exec(const char* cmd) {
 int main(void)
 {
 	std::string response, command;
-	std::string line, param_name, param_content, token, bot_name, lang;
+	std::string line, param_name, param_content, token, bot_name, lang, scripts_path, aux_keywords, aux_script, aux_param;
+	std::list<cmd> available_commands;
 	std::ifstream configFile("../config");
 	if(configFile.is_open())
 	{
@@ -37,13 +46,51 @@ int main(void)
 			int div_pos = line.find_last_of("=");
 			param_name = line.substr(0,div_pos);
 			param_content = line.substr(div_pos+1);
-			if(!param_name.compare("TOKEN")) token = param_content;
+			if(line.empty());	//ignore empty lines
+			else if(!param_name.compare("TOKEN")) token = param_content;
 			else if(!param_name.compare("BOT_NAME")) bot_name = param_content;
 			else if(!param_name.compare("LANGUAGE")) lang = param_content;
-			else std::cout << "Unknown parameter in configuration file: " << line << std::endl;
+			else if(!param_name.compare("SCRIPTS_PATH")) scripts_path = param_content;
+			else if(!param_name.compare("COMMAND"))
+			{
+				std::cout << "Configuring command..." << std::endl;
+				while(line.compare("}"))
+				{
+					if(std::getline(configFile,line))
+					{
+						div_pos = line.find_last_of("=");
+						param_name = line.substr(0,div_pos);
+						param_content = line.substr(div_pos+1);
+						if(!param_name.compare("KEYWORDS")) aux_keywords = param_content;
+						else if(!param_name.compare("SCRIPT")) aux_script = param_content;
+						else if(!param_name.compare("PARAMETERS")) aux_param = param_content;
+					}else
+					{
+						std::cout << "Unknown parameter in configuration file: " << line << std::endl;
+						return EXIT_FAILURE;
+					}
+				}
+				cmd aux_cmd = {
+					aux_keywords,
+					aux_script,
+					aux_param
+				};
+				available_commands.push_front(aux_cmd);
+				std::cout << "Command configuration successful: \""+aux_cmd.key+"\"" << std::endl;
+				std::cout << "Script: \""+scripts_path+aux_cmd.script+"\"" << std::endl;
+				std::cout << "Parameters: \""+aux_cmd.param+"\"" << std::endl;
+			}else
+			{
+				std::cout << "Unknown parameter in configuration file: " << line << std::endl;
+				return EXIT_FAILURE;
+			}
 		}
 		configFile.close();
-	}else std::cout << "Error reading configuration file" << std::endl;
+	}else
+	{
+		std::cout << "Error reading configuration file" << std::endl;
+		return EXIT_FAILURE;
+	}
   	telegram::sender sender(token);                                               // create a sender with our token for outgoing messages
   	telegram::listener::poll listener(sender);                                    // create a polling listener which will process incoming requests, polling using the sender
   	listener.set_callback_message([&](telegram::types::message const &message)	  // we set a callback for receiving messages in native format, using a lambda for convenience
@@ -80,7 +127,7 @@ int main(void)
 				system(std::string("rm "+file_name+" "+file_wav+" transcript").c_str());
 			}else sender.send_message(message_chat_id, "Error downloading voice message");
 		}
-		response = checknrun(command);
+		response = checknrun(command,available_commands,scripts_path);
 		sender.send_message(message.chat.id, response);
   	});
 	listener.set_num_threads(1);
@@ -107,28 +154,14 @@ int transcribe(std::string file_name)
 	return system(std::string("/usr/local/bin/spchcat "+file_name+" >> transcript").c_str());
 }
 
-std::string checknrun(std::string command)
+std::string checknrun(std::string command, std::list<cmd> available_commands, std::string scripts_path)
 {
-	std::string response;
+	std::string response = "No entiendo que quiere decir \""+command+"\" bro";
 	if(command.empty()) response = "Error de transcripción. Intentá nuevamente con un audio más largo.";
-	else if(command == "Battery")
+	else for (std::list<cmd>::iterator it = available_commands.begin(); it != available_commands.end(); it++)
 	{
-		response = exec("../scripts/battery-status/battery-status.sh");
+		if (command == it->key)
+			response = exec(std::string(scripts_path+it->script+" "+it->param).c_str());
 	}
-	else if (command == "Start")
-	{
-		response = exec("../scripts/serial-commander/sc start");
-		
-	}
-	else if (command == "prender la luz de la cocina")
-	{
-		response = exec("../scripts/serial-commander/sc led_on");
-		if(!response.compare("Successful connection to /dev/ttyUSB0\n")) response  = "Ya te la prendí maestro";	//Si la operación sale bien, cambia la respuesta por otro mensaje. Si sale mal, muestra el error correspondiente que ya se encuentra en el string response.
-	}
-	else if (command == "apagar la luz de la cocina")
-	{
-		response = exec("../scripts/serial-commander/sc led_off");
-		if(!response.compare("Successful connection to /dev/ttyUSB0\n")) response  = "Ya te la apagué maestro";
-	}else response = "No entiendo que quiere decir \""+command+"\" bro";
 	return response;
 }
